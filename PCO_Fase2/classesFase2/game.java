69025d3b82bedef627ee525751e440eb7c8ada15
Diogo <diogobailaoforte@gmail.com>
2022-11-08 18:51:47 +0000

    ihiovh

---
+++ PCO_Fase2/classesFase2/game.java
@@ -0,0 +1,10 @@
+/*
+ * @author Diogo Forte nº 56931, Tiago Pereira nº55854
+ */
+
+public class game {
+    
+ 
+
+    
+}
---
+++ PCO_Fase2/classesFase2/piece.java
@@ -0,0 +1,9 @@
+/*
+ * @author Diogo Forte nº 56931, Tiago Pereira nº55854
+ */
+import generator;
+
+public class piece {
+    private Symbol[] p;
+    
+}
