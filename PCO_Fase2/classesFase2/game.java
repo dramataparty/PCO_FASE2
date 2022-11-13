af4bb1b558ab98e6804403c58ff209404420cb57
Diogo <diogobailaoforte@gmail.com>
2022-11-13 17:47:54 +0000

    progress1

--- PCO_Fase2/classesFase2/game.java
+++ PCO_Fase2/classesFase2/game.java
@@ -1,10 +1,45 @@
-/*
- * @author Diogo Forte nº 56931, Tiago Pereira nº55854
- */
-
-public class game {
+public class Game {
     
- 
+
+    int linesInGrid() = ;
+        
+        
 
     
+    int colsInGrid() = ;
+
+
+
+
+    public static boolean validGrid(Symbol[][] g){
+        boolean valgrid = true;
+        int colcount = 0;
+        for(Symbol [] i:g){
+            colcount++;
+            if(i.length != linesInGrid){
+                valgrid = false;
+            }
+
+
+
+            for(int e = 0; e < i.length - 1;e++){
+                if(i[e]==null || ( (i[e] != Symbol.EMPTY) && i[e+1]== Symbol.EMPTY )){
+                    valgrid = false;
+                }
+            }
+        if(colcount!=colsInGrid){
+            valgrid = false;
+        }
+
+        }
+        return valgrid;
+
+    }
+
+
+
+    public static void main(String[] args){
+        
+
+    }
 }
--- PCO_Fase2/classesFase2/piece.java
+++ PCO_Fase2/classesFase2/piece.java
@@ -1,7 +1,6 @@
 /*
  * @author Diogo Forte nº 56931, Tiago Pereira nº55854
  */
-import generator;
 
 public class piece {
     private Symbol[] p;
